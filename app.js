4e1fad1498ad28ef7afc98d1179d8a9e4aa2342e
JBarrett0329 <barretja@purdue.edu>
2024-04-29 12:11:30 -0400

    BASIC REPORTS DONE

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const Run = require('./models/run');
 const User = require('./models/user');
 const { request } = require('http');
+
 
 
 // express app
@@ -26,6 +28,8 @@ app.set('view engine', 'ejs');
 // middleware & static files
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.json()); // Parse JSON request body
 
 app.use(morgan('dev'));
 app.use((req, res, next) => {
@@ -106,6 +110,7 @@ app.post('/new-user', (req, res) => {
     password: req.body.password
   });
 
+  res.cookie('userId', user._id);
   user.save()
     .then(result => {
         res.redirect('/runs');
@@ -133,6 +138,7 @@ app.post('/sign-in', async (req, res) => {
       if (password != user.password) {
           return res.status(401).json({ message: 'Incorrect password' });
       }
+      res.cookie('userId', user._id);
 
       res.redirect('/runs');
   } catch (err) {
@@ -145,18 +151,94 @@ app.get('/runs/create', (req, res) => {
   res.render('create', { title: 'Log a new run' });
 });
 
+
 app.get('/runs', (req, res) => {
-  Run.find().sort({ createdAt: -1 })
-    .then(result => {
-      res.render('index', { runs: result, title: 'All runs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  const userId = req.cookies.userId;
+  const filter = {};
+  filter.user = userId;
+
+  console.log(req.query.distance);
+  if (req.query.distance) {
+      filter.distance = { $gte: parseFloat(req.query.distance) };
+  } else if (req.query.date) {
+    filter.date = req.query.date;
+  } else if (req.query.duration) {
+    filter.duration = { $gte: req.query.duration };
+  } else if (req.query.pace) {
+    filter.pace = { $gte: parseFloat(req.query.pace)* 60 };
+    console.log(filter.pace);
+  }
+
+  Run.find(filter).sort({ createdAt: -1 })
+      .then(result => {
+        if (req.originalUrl.includes('?')) {
+          console.log(req.query.option, req.query.value);
+          res.render('index', { runs: result, title: 'Filtered runs'});
+        } else {
+          console.log(req.query.option, req.query.value);
+          res.render('index', { runs: result, title: 'All runs'});
+        }
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).send('Error fetching runs');
+      });
 });
 
+function parseDurationToSeconds(duration) {
+  const parts = duration.split(':'); // Split the duration string into parts
+  
+  let seconds = 0;
+  
+  // Convert hours, minutes, and seconds to seconds and sum them up
+  if (parts.length === 3) {
+    seconds += parseInt(parts[0]) * 3600; // Hours to seconds
+    seconds += parseInt(parts[1]) * 60; // Minutes to seconds
+    seconds += parseInt(parts[2]); // Seconds
+  } else if (parts.length === 2) {
+    seconds += parseInt(parts[0]) * 60; // Minutes to seconds
+    seconds += parseInt(parts[1]); // Seconds
+  } else if (parts.length === 1) {
+    seconds += parseInt(parts[0]); // Seconds
+  }
+
+  return seconds;
+}
+
+function formatDuration(durationInput) {
+  const durationValue = durationInput;
+
+  // Split the duration string by ':'
+  const parts = durationValue.split(':');
+
+  if (parts.length === 2) {
+      // Duration does not include hours, add '0:' to the beginning
+      return `0:${durationValue}`;
+  } else if (parts.length === 3) {
+      // Duration already includes hours
+      return durationValue;
+  } else {
+      // Invalid duration format
+      console.error('Invalid duration format');
+  }
+}
+
 app.post('/runs', (req, res) => {
-  const run = new Run(req.body);
+  const userId = req.cookies.userId;
+  console.log(userId);
+
+  const { distance, duration } = req.body;
+  const dist = parseFloat(distance);
+  const durationInSeconds = parseDurationToSeconds(duration);
+  const pace = durationInSeconds / dist;
+  const newDuration = formatDuration(duration)
+
+  const run = new Run({
+      ...req.body,
+      duration: newDuration,
+      pace: pace,
+      user: userId
+  });
   
   run.save()
     .then(result => {
@@ -166,6 +248,16 @@ app.post('/runs', (req, res) => {
       console.log(err);
     });
 });
+
+
+
+app.post('/apply-filter', (req, res) => {
+  const { option, value } = req.body;
+  console.log(option, value);
+  // Redirect to /runs with the filter option and value as query parameters
+  res.redirect(`/runs?${option}=${encodeURIComponent(value)}`);
+});
+
 
 app.post("/runs/:id", async (req, res) => {
   const { id } = req.params;
